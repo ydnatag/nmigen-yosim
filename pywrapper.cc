ec3df6ca7f46020bd2fb67a380b66fa19fb1e5f7
Andres Demski <andresdemski@gmail.com>
2019-12-04 18:40:50 -0300

    get signal names

--- pywrapper.cc
+++ pywrapper.cc
@@ -27,9 +27,15 @@ struct signal_handler_t sig_handler[] = {
     {.id = 0, .name = "rst", .width = 1,  .signal = (void*) &top.rst, .setter = set_next_value<1>,  .getter = get_current_value<1> },
     {.id = 1, .name = "clk", .width = 1,  .signal = (void*) &top.clk, .setter = set_next_value<1>,  .getter = get_current_value<1> },
     {.id = 2, .name = "r",   .width = 11, .signal = (void*) &top.r,   .setter = set_next_value<11>, .getter = get_current_value<11>},
-    {.id = 3, .name = "b",   .width = 10, .signal = (void*) &top.b,   .setter = set_next_value<10>, .getter = get_current_value<10> },
+    {.id = 3, .name = "b",   .width = 10, .signal = (void*) &top.b,   .setter = set_next_value<10>, .getter = get_current_value<10>},
     {.id = 4, .name = "a",   .width = 10, .signal = (void*) &top.a,   .setter = set_next_value<10>, .getter = get_current_value<10>},
 };
+
+static PyObject* get_signal_name(PyObject *self, PyObject *args) {
+    uint32_t id;
+    PyArg_ParseTuple(args,"i", &id);
+    return Py_BuildValue("s", sig_handler[id].name);
+}
 
 static PyObject* set_by_id(PyObject *self, PyObject *args) {
     uint32_t id;
@@ -45,11 +51,6 @@ static PyObject* get_by_id(PyObject *self, PyObject *args) {
     return Py_BuildValue("i", sig_handler[id].getter(sig_handler[id].signal));
 }
 
-static PyObject* hello_world(PyObject *self, PyObject *args) {
-    printf("Hello, world!\n");
-    Py_RETURN_NONE;
-}
-
 static PyObject* delta(PyObject *self, PyObject *args) {
 	do {
 		top.eval();
@@ -57,9 +58,20 @@ static PyObject* delta(PyObject *self, PyObject *args) {
     Py_RETURN_NONE;
 }
 
+static PyObject* n_of_signals(PyObject *self, PyObject *args) {
+    return Py_BuildValue("i", sizeof(sig_handler)/sizeof(struct signal_handler_t));
+}
+
+static PyObject* eval(PyObject *self, PyObject *args) {
+    top.eval();
+    Py_RETURN_NONE;
+}
+
 static PyMethodDef simulator_methods[] = { 
-    {"hello_world", hello_world, METH_NOARGS, "Print 'hello world' from a method defined in a C extension." },
+    {"n_of_signals", n_of_signals, METH_NOARGS, "delta step"},
+    {"get_signal_name", get_signal_name, METH_VARARGS, "set value by signal id"},
     {"delta", delta, METH_NOARGS, "delta step"},
+    {"eval", eval, METH_NOARGS, "delta step"},
     {"set_by_id", set_by_id, METH_VARARGS, "set value by signal id"},
     {"get_by_id", get_by_id, METH_VARARGS, "get value by signal id"},
     {NULL, NULL, 0, NULL}
