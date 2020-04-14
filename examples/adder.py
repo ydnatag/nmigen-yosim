dfbe96e836518ea039c907d4032aaf54fa33bf9c
Andres Demski <andresdemski@gmail.com>
2020-04-14 19:32:54 -0300

    example updated

--- examples/adder.py
+++ examples/adder.py
@@ -1,5 +1,6 @@
 from nmigen import *
 from nmigen_yosim import *
+from nmigen_yosim.vcd import VCDWaveformWriter
 import random
 import time
 
@@ -8,6 +9,7 @@ class Add(Elaboratable):
         self.a = Signal(width)
         self.b = Signal(width)
         self.r = Signal(width + 1)
+
     def elaborate(self, platform):
         m = Module()
         m.d.comb += self.r.eq(self.a + self.b)
@@ -24,7 +26,6 @@ class Adder(Elaboratable):
     def elaborate(self, platform):
         m = Module()
         m.submodules.adder = add = Add(self.width)
-        
         m.d.comb += add.a.eq(self.a)
         m.d.comb += add.b.eq(self.b)
         m.domain[self.d] += self.r.eq(add.r)
@@ -33,36 +34,44 @@ class Adder(Elaboratable):
 def reset_coroutine(rst, clk):
     rst.value = 1
     yield rising_edge(clk)
-    rst.value = 1
-    yield rising_edge(clk)
     rst.value = 0
+    yield rising_edge(clk)
 
 def main_coroutine(sim, dut):
-    yield from reset_coroutine(dut.rst, dut.clk)
     for j in range(8, 65):
-        for i in range(100):
+        yield from reset_coroutine(dut.rst, dut.clk)
+        for i in range(1000):
             a = random.randint(0, 2**j-1)
-            dut.a.value = a
             b = random.randint(0, 2**j-1)
+            dut.a.value = a
             dut.b.value = b
             yield rising_edge(dut.clk)
             yield rising_edge(dut.clk)
             assert a == dut.a.value, f'{a} == {dut.a.value}'
             assert b == dut.b.value, f'{b} == {dut.b.value}'
-            assert a + b == dut.r.value, f'@{sim.sim_time} ps: {a} + {b} == {dut.r.value}'
+            assert a + b == dut.r.value, (
+                f'@{sim.sim_time} ps: {a} + {b} == {dut.r.value}')
 
-if __name__ == '__main__':
+def run_sim(vcd):
     m = Adder(65, 'sync')
-    ports = [m.a, m.b, m.r]
-    with Simulator(m, platform=None, ports=ports) as (sim, dut):
-        sim.set_precision(1, 'ns')
-        start = time.time()
+    sim_config = {
+        'platform': None,
+        'ports': [m.a, m.b, m.r],
+        'vcd_file': './dump.vcd' if vcd else None,
+        'precision': (5, 'ns')}
+
+    start = time.time()
+    with Simulator(m, **sim_config) as (sim, dut):
         clock_coro = clock(dut.clk, 10, 'ns')
         main_coro = main_coroutine(sim, dut)
         sim.run([clock_coro, main_coro])
-        elapsed = time.time() - start
+    elapsed = time.time() - start
 
-    print('\nResults:')
+    print(f'\nResults (vcd={vcd}):')
     print(f'sim time: {sim.sim_time / 1000} ns')
     print(f'real time: {elapsed} s')
     print(f'simtime / realtime: {sim.sim_time / 1000 / elapsed} ns/s')
+
+if __name__ == '__main__':
+    run_sim(vcd=True)
+    run_sim(vcd=False)
