acdc6334f62098a27ed34ac280c741fbdb39f517
Andres Demski <andresdemski@gmail.com>
2020-04-15 14:34:10 -0300

    clock start value

--- nmigen_yosim/coro.py
+++ nmigen_yosim/coro.py
@@ -1,7 +1,8 @@
 from .trg import timer
 
-def clock(clk, period=10, units='ps'):
+def clock(clk, period=10, units='ps', start_value=1):
     period_2 = int(period / 2)
+    clk.value = start_value
     while True:
         yield timer(period_2, units=units)
         clk.value = (clk.value + 1) % 2
